9a25ffc156599a0ff29e0ed79ca02f9a10a2a158
Esanic <Tofflund@hotmail.com>
2022-04-06 21:34:02 +0200

    Löst sparande av tärning och lagt till möjlighet att spara eller avsluta med nuvarnade tärningar.

--- Yahtzee/Program.cs
+++ Yahtzee/Program.cs
@@ -9,7 +9,7 @@ namespace Yahtzee
         {
             int counter = 0;
 
-            
+            Console.Clear();
             Console.WriteLine("Spela!");
 
             Dice one = new (Dice.Position.One);
@@ -18,17 +18,16 @@ namespace Yahtzee
             Dice four = new(Dice.Position.Four);
             Dice five = new(Dice.Position.Five);
 
-            //Dice[] activeDices = {one, two, three, four, five};
-            //Dice[] savedDices = Array.Empty<Dice>();
             List<Dice> active = new() { one, two, three, four, five  };
             List<Dice> saved = new();
-
             
             ActiveDices ActiveDices = new(active);
             SavedDices SavedDices = new(saved, ActiveDices);
 
             while (counter < 3)
             {
+
+                Console.Clear();
 
                 if (SavedDices.Dices.Count > 0)
                 {
@@ -37,9 +36,10 @@ namespace Yahtzee
                 }
 
                 Console.ReadLine();
-                Console.WriteLine("Tärningarna kastas...");
                 counter++;
-
+                
+                Console.WriteLine($"Kast nr {counter}");
+                
                 foreach (Dice dice in ActiveDices.Dices)
                 {
                     Dice.HitDices(dice);
@@ -47,33 +47,50 @@ namespace Yahtzee
 
                 ActiveDices.TypeActiveDices();
 
-                Console.WriteLine("Vill du spara tärningar?");
-                string answer = Console.ReadLine().ToLower();
-
-                while (answer == "ja" || answer == "j")
+                if (counter < 3)
                 {
-                    Console.Clear();
+                    Console.WriteLine("1. Spara tärningar");
+                    Console.WriteLine("2. Avsluta och välj resultat");
+                    Console.Write("Ditt val: ");
+                    string answer = Console.ReadLine().ToLower();
 
-                    ActiveDices.TypeActiveDices();
+                    if (answer == "1")
+                    {
+                        while (true)
+                        {
+                            Console.Clear();
 
-                    Console.WriteLine("Vilken tärning vill du spara?");
-                    string diceChoice = Console.ReadLine();
+                            ActiveDices.TypeActiveDices();
 
-                    int.TryParse(diceChoice, out int diceToBeSaved);
+                            Console.WriteLine("Vilken tärning vill du spara?");
+                            string diceChoice = Console.ReadLine();
 
-                    SavedDices.Dices.Add(ActiveDices.Dices.Find(x => (int)x.position == diceToBeSaved - 1));
+                            int.TryParse(diceChoice, out int diceToBeSaved);
 
-                    //SavedDices.Dices.Add(ActiveDices.Dices[diceToBeSaved - 1]);
-                    //ActiveDices.Dices.Remove(ActiveDices.Dices[diceToBeSaved - 1]);
+                            SavedDices.Dices.Add(ActiveDices.Dices.Find(x => (int)x.position == diceToBeSaved));
+                            ActiveDices.Dices.Remove(ActiveDices.Dices.Find(x => (int)x.position == diceToBeSaved));
 
-                    Console.WriteLine("Sparade tärningar:");
-                    SavedDices.TypeSavedDices();
+                            Console.WriteLine("Sparade tärningar:");
+                            SavedDices.TypeSavedDices();
 
-                    Console.WriteLine("Vill du spara fler?");
-                    answer = Console.ReadLine().ToLower();
+                            Console.WriteLine("Vill du spara fler?");
+                            Console.WriteLine("1. Ja");
+                            Console.WriteLine("2. Nej");
+                            Console.Write("Ditt val: ");
+                            string a = Console.ReadLine().ToLower();
+
+                            if (a == "2")
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Välj var du vill lägga ditt resultat");
                 }
             }
-
         }
     }
 }
